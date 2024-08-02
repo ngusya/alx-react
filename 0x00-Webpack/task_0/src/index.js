47ecf13b345133e73cac0d56bd19d08144c41a0c
Carthage <carthage@ubuntu.myguest.virtualbox.org>
2024-08-02 11:29:58 +0300

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+const $ = required( "jquery" );
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard dataa for the students</p>');
+$('body).append('<p>Copyright - Holbert School</p>');
